ec892fd9ef5ba9e515c875c69cbb637c5341804d
Ayush04Barai <arbarai371322@kkwagh.edu.in>
2026-01-20 10:42:34 +0530

    Updated

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AttachmentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
-use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
